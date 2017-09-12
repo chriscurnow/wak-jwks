589585f135de8033f3470a0109211b2bb95a0b72
Chris Curnow <chris@4cast.com.au>
2017-09-12 14:38:42 +1000

    Minor changes preparing for release

--- index.js
+++ index.js
@@ -2,22 +2,38 @@
 
 module.exports = verify;
 
-const getPem = require('rsa-pem-from-mod-exp');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
 
-
+// @token jwt token
+// @options options to pass to the jwksClient
+//      - currently only supports jwksUri
+// @cb - callback function
+//      - if successful returns the decoded token
+//      - if an error occurs it returns the Error
 function verify(token, options, cb){
 
-const client = jwksClient({
-  strictSsl: true, // Default value
-  jwksUri: options.jwksUri//'https://4cast-pro.au.auth0.com/.well-known/jwks.json'
-});
+  // jwksClient goes to the specified uri to get the signing key
+  // once we have the signing key we can use it to
+  // verify the token
+
+    const client = jwksClient({
+      cache: true,
+      cacheMaxEntries: 5, // Default value
+      cacheMaxAge: ms('10h'), // Default value
+      rateLimit: true,
+      jwksRequestsPerMinute: 10, // Default value
+      strictSsl: true, // Default value
+      jwksUri: options.jwksUri
+    });
 
 
-client.getSigningKey(options.kid, (err, key) => {
+    client.getSigningKey(options.kid, (err, key) => {
 
-		 	const signingKey = key.publicKey || key.rsaPublicKey;
+		 	      const signingKey = key.publicKey || key.rsaPublicKey;
+
+            // now use the key to verify and decode the token
+
             jwt.verify(token, signingKey, function(err, decoded){
              if(err){
                 return cb(err);
@@ -29,8 +45,5 @@ client.getSigningKey(options.kid, (err, key) => {
 
            });
 
-
-
-  // Now I can use this to configure my Express or Hapi middleware
 });
 }
